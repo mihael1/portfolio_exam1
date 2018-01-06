5150b3eff48e948202e9bd88e5acd0c98a25fece
Mihael Sandro <Mihael@mihaels-mbp.home>
2018-01-06 23:52:02 +0100

    first comit

---
+++ javas.js
@@ -0,0 +1,30 @@
+/* let menu = document.querySelector("#myNav");
+
+menu.addEventListener('mouseover', openMenu);
+
+function openMenu() {
+    console.log("open menu bro");
+
+
+}
+
+
+ Open when someone clicks on the span element
+function openNav() {
+    document.querySelector("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+
+menu.addEventListener('mouseover', colorComes);
+
+function colorComes() {
+    console.log("broski");
+    bruh.classList.add("retardcolor");
+    menu.addEventListener("mouseout", colorGoes)*/
