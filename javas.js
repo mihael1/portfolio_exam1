19af34346447602c7b414de357621a00fe9689c9
Mihael Sandro <Mihael@mihaels-mbp.home>
2018-01-07 02:09:29 +0100

    navigation done wuhuu lets sleep

--- javas.js
+++ javas.js
@@ -14,7 +14,7 @@ function openNav() {
     document.querySelector("myNav").style.width = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay
+Close when someone clicks on the "x" symbol inside the overlay
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
@@ -28,3 +28,13 @@ function colorComes() {
     console.log("broski");
     bruh.classList.add("retardcolor");
     menu.addEventListener("mouseout", colorGoes)*/
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '100%';
+    document.getElementById('burger').style.visibility = 'hidden';
+}
+
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('burger').style.visibility = 'visible';
+
+}
